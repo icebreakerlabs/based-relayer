218ea49b8aa7482ebe2b07bb15a2397e3e0261fb
icebreakerlabs <124850741+icebreakerlabs@users.noreply.github.com>
2023-03-22 18:11:08 -0400

    fix: update app.tsx activechain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, useContract } from "@thirdweb-dev/react";
+import { BaseGoerli } from "@thirdweb-dev/chains";
 import Head from "next/head";
 import { ChakraProvider } from '@chakra-ui/react';
 
@@ -10,7 +11,7 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <ThirdwebProvider activeChain={env.activeChain}>
+      <ThirdwebProvider activeChain={BaseGoerli}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
@@ -18,4 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </ThirdwebProvider>
     </ChakraProvider>
   );
+}
+function Component() {
+  const { contract, isLoading } = useContract("0x38e6F1000cB611B06edFF9c271aEF058675a6c2f");
 }
